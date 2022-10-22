ae5329eac6dc3bdf1b49843ca200436777b9e6d4
Douppen <simon.nars@gmail.com>
2022-10-22 11:47:50 +0300

    made 3d board dynamic based on the game state passed as props to the component

--- src/pages/board.tsx
+++ src/pages/board.tsx
@@ -1,15 +1,18 @@
 import ChessModel from "../components/ChessModel";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { useState } from "react";
+import { Chess } from "chess.js";
 
 const GamePage = () => {
+  const [gameState, setGameState] = useState(new Chess());
   return (
     <div id="canvas-container" style={{ height: "100vh" }}>
-      <Canvas camera={{ position: [5, 10, 10], fov: 50 }}>
+      <Canvas camera={{ position: [-5, 8, -8], fov: 50 }}>
         <hemisphereLight color="white" intensity={0.45} />
         <spotLight position={[50, 50, 10]} angle={0.15} penumbra={1} />
         <group position={[4, 0, -3]}>
-          <ChessModel />
+          <ChessModel boardPosition={gameState.board()} />
         </group>
         <OrbitControls />
       </Canvas>
---
+++ src/types/types.ts
@@ -0,0 +1,4 @@
+export interface ChessVec {
+  x: number;
+  y: number;
+}
