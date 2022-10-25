a9603ea8931b190d18325e0ba2bbad53fed9beb0
Douppen <simon.nars@gmail.com>
2022-10-25 16:18:44 +0300

    removed example routers from trpc app router

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,14 +1,10 @@
 // src/server/trpc/router/_app.ts
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
 import { authRouter } from "./auth";
-import { questionRouter } from "./question";
 import { gameRouter } from "./game";
 
 export const appRouter = router({
-  example: exampleRouter,
   auth: authRouter,
-  question: questionRouter,
   game: gameRouter,
 });
 
