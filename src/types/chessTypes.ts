cc06e25336186b518eb97d7354a194f47c68b2bb
Douppen <simon.nars@gmail.com>
2022-10-25 13:43:20 +0300

    more schema tweaks: user info, game info, moves history

--- src/types/chessTypes.ts
+++ src/types/chessTypes.ts
@@ -1,4 +1,34 @@
+import { PieceSymbol, Square } from "chess.js";
+
 export interface ChessVec {
   x: number;
   y: number;
+}
+
+export interface MoveInfo {
+  color: "white" | "black";
+  from: Square;
+  to: Square;
+  piece: PieceSymbol;
+  san: string;
+  flags: Flag[];
+  captured?: PieceSymbol;
+  promotion?: "n" | "b" | "r" | "q";
+
+  remainingMillis: number;
+  endTimeMillis: number;
+
+  fenAfterMove: string;
+}
+
+export type MovesList = MoveInfo[];
+
+export enum Flag {
+  NORMAL = "n",
+  CAPTURE = "c",
+  BIG_PAWN = "b",
+  EP_CAPTURE = "e",
+  PROMOTION = "p",
+  KSIDE_CASTLE = "k",
+  QSIDE_CASTLE = "q",
 }
